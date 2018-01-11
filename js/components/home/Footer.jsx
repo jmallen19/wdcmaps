da5678662fc861c88d73e7ccd7c89a95c6255503
jmallen19 <35009284+jmallen19@users.noreply.github.com>
2018-01-12 09:50:46 +1300

    footer mods

--- js/components/home/Footer.jsx
+++ js/components/home/Footer.jsx
@@ -6,8 +6,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 var React = require('react');
-//var MailingLists = require('./MailingLists');
-//const MadeWithLove = require('../../assets/img/mwlii.png');
 
 class Footer extends React.Component {
     render() {
@@ -15,9 +13,6 @@ class Footer extends React.Component {
             <div id="footer" style={{
                 textAlign: "center"
             }}>
-                //<MailingLists/>
-                //<br />
-                //<img src={MadeWithLove} />
                 <p align="center"><b><a href="http://www.wairoadc.govt.nz">Wairoa District Council</a></b> • Coronation Square, Queen Street, Wairoa - P O Box 54, Wairoa 4160, Hawke's Bay - New Zealand</p>
                 <p align="center"><a href="mailto:administrator@wairoadc.govt.nz">administrator@wairoadc.govt.nz</a> • <a href="http://www.wairoadc.govt.nz">www.wairoadc.govt.nz</a> • Tel: +64 6 838 7309 • Fax: +64 6 838 8874</p>
             </div>
